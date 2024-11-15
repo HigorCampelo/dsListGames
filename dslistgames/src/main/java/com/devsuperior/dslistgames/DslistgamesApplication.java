4ae4ab42dd1b923de27ca599dee06016421a90e4
Higor Campelo <c0604368@vale.com>
2024-11-15 16:17:07 -0300

    Project created

---
+++ dslistgames/src/main/java/com/devsuperior/dslistgames/DslistgamesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistgamesApplication.class, args);
+	}
+
+}
---
+++ dslistgames/src/test/java/com/devsuperior/dslistgames/DslistgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
